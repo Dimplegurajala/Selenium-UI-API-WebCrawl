419d76c450fababdb62a12df91d65c023f47808b
Dimplegurajala <dimplegurajala@gmail.com>
2025-12-26 03:21:16 +0530

    added-__init__.py files and modified conftest.py for headlessmode

--- conftest.py
+++ conftest.py
@@ -6,10 +6,10 @@ from selenium.webdriver.chrome.service import Service
 def driver(request):
     options = webdriver.ChromeOptions()
     # integration of GIT-- CI/CD
-    #options.add_argument("--headless=new")
-    #options.add_argument("--no-sandbox")
-    #options.add_argument("--disable-dev-shm-usage")
-    #options.add_argument("--disable-gpu")
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
     
     #  bypassing 'Bot Detection' on sites
     options.add_argument("--disable-blink-features=AutomationControlled")
